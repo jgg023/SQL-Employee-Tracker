4f87284cbf0a15ad19654f298338c5371c418630
Jgg023 <mightyfresh1@gmail.com>
2023-01-25 21:18:02 -0500

    startup

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS movies_db;
+CREATE DATABASE movies_db;
+
+USE movies_db;
+
+CREATE TABLE movies_db;(
+  id INT ,
+  movie_name VARCHAR(30) NOT NULL,
+  primary id
+
+CREATE TABLE reviews_db;(
+  id INT ,
+  movie_id VARCHAR(30) NOT NULL,
+  review_details TEXT VARCHAR(30) TEXT,
+  FOREIGN KEY (reviews_db)
+  REFRENCES review(db)
+  ON DELETE SET NULL
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'courses_db'
+  },
+  console.log(`Connected to the courses_db database.`)
+);
+
+// Hardcoded query: DELETE FROM course_names WHERE id = 3;
+
+db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
